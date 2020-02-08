654e6cac1f8b58aa614f1986d30a3dd6458a4327
JimiDeSoto <jimidesoto@overseer.pl>
2020-02-08 23:27:11 +0100

    202002082327

---
+++ awsArchitectAssociate/week6/ec2-start.py
@@ -0,0 +1,39 @@
+import boto3
+import logging
+
+#setup simple logging for INFO
+logger = logging.getLogger()
+logger.setLevel(logging.INFO)
+
+#define the connection
+ec2 = boto3.resource('ec2')
+
+def lambda_handler(event, context):
+    # Use the filter() method of the instances collection to retrieve
+    # all running EC2 instances.
+    filters = [{
+            'Name': 'tag:Env',
+            'Values': ['TEST']
+        },
+        {
+            'Name': 'instance-state-name', 
+            'Values': ['stopped']
+        }
+    ]
+    
+    #filter the instances
+    instances = ec2.instances.filter(Filters=filters)
+
+    #locate all running instances
+    RunningInstances = [instance.id for instance in instances]
+    
+    #print the instances for logging purposes
+    #print RunningInstances 
+    
+    #make sure there are actually instances to shut down. 
+    if len(RunningInstances) > 0:
+       
+        startingUp = ec2.instances.filter(InstanceIds=RunningInstances).start()
+        print startingUp
+    else:
+        print "Nothing to see here"
